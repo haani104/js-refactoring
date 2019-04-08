1bbad96bafa5b970f4913227c850f157d7e2ebe0
haani <haani.rizvi@tokopedia.com>
2019-04-08 17:57:56 +0530

    Adding type polymorphism.

--- statement.js
+++ statement.js
@@ -1,12 +1,20 @@
 const plays = require("./plays.json");
 const invoices = require("./invoices.json");
-const createStatementData = require('./createStatementData.js')
+const createStatementData = require("./createStatementData.js");
 
 function statement(invoice, plays) {
   renderPlainText(createStatementData(invoice, plays));
 }
 
 statement(invoices[0], plays);
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format(aNumber / 100);
+}
 
 function renderPlainText(data) {
   let result = `Statement for ${data.customer}\n`;
@@ -18,12 +26,21 @@ function renderPlainText(data) {
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   console.log(result);
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2
-    }).format(aNumber / 100);
+function htmlStatement(invoice, plays) {
+  renderHTML(createStatementData(invoice, plays));
+}
+
+function renderHTML(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>\n`;
+  result += "<table"
+  for (let perf of data.performances) {
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${
+      perf.audience
+    } seats)\n`;
   }
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
+  console.log(result);
 }
