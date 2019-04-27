e0a69dca7b0ee24e0804229e7d5b0622319ff612
haani <haani.rizvi@tokopedia.com>
2019-04-27 16:28:06 +0700

    created volumeCredits as class get function

--- createStatementData.js
+++ createStatementData.js
@@ -24,6 +24,16 @@ class PerformanceCalculator {
     }
     return result;
   }
+
+  get volumeCredits() {
+    let result = 0;
+    result += Math.max(this.performance.audience - 30, 0);
+    //add extra credit for for every ten comedy attendees
+    if ("comedy" === this.play.type) {
+      result += Math.floor(this.performance.audience / 5);
+    }
+    return result;
+  }
 }
 
 function createStatementData(invoice, plays) {
@@ -38,8 +48,8 @@ function createStatementData(invoice, plays) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = calculator.play;
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
@@ -48,17 +58,11 @@ function createStatementData(invoice, plays) {
   }
 
   function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
   }
 
   function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    //add extra credit for for every ten comedy attendees
-    if ("comedy" === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
   }
 
   function totalAmount(data) {
