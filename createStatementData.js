fc01324316a42f3b6cd5afc698d1c7397df788bc
haani <haani.rizvi@tokopedia.com>
2019-04-27 16:09:28 +0700

    Creating amount as get inside the PerformanceCalculator class

--- createStatementData.js
+++ createStatementData.js
@@ -3,6 +3,27 @@ class PerformanceCalculator {
     this.performance = aPerformance;
     this.play = aPlay;
   }
+
+  get amount() {
+    let result = 0;
+    switch (aPerformance.play.type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 2000 * (aPerformance.audience - 20);
+        }
+        break;
+      default:
+        throw new Error(`unknown type ${aPerformance.play.type}`);
+    }
+    return result;
+  }
 }
 
 function createStatementData(invoice, plays) {
